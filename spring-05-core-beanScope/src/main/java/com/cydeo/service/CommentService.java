b58ed564728d3dcbfd380c6a40ff19614b6bd141
ViolettaMal <malakhovskaya@yahoo.com>
2024-03-09 16:08:42 -0600

    beanScope

---
+++ spring-05-core-beanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,28 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.proxy.EmailCommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import com.cydeo.repository.DBCommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository;
+
+    //    public CommentService(@Qualifier("emailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
+    public CommentService(@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
+        this.commentNotificationProxy = commentNotificationProxy;
+        this.commentRepository = commentRepository;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+
+    }
+}
