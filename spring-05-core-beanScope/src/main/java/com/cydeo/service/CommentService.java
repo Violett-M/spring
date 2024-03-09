eff878900ac0bc370fd22433899153764bf3581c
ViolettaMal <malakhovskaya@yahoo.com>
2024-03-09 16:32:51 -0600

    beanScope

--- spring-05-core-beanScope/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-core-beanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -6,9 +6,15 @@ import com.cydeo.proxy.EmailCommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import com.cydeo.repository.DBCommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype") //whenever somebody requests CommentService bean, I want different beans to be created in the container
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE) // Just another way. It does the same as above.
+@Lazy
 public class CommentService {
 
     private final CommentNotificationProxy commentNotificationProxy;
@@ -18,6 +24,7 @@ public class CommentService {
     public CommentService(@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
         this.commentNotificationProxy = commentNotificationProxy;
         this.commentRepository = commentRepository;
+        System.out.println("Hello");
     }
 
     public void publishComment(Comment comment){
