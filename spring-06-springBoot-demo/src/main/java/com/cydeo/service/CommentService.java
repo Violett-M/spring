2b26d7632a4246fac7a88511985139141482971b
ViolettaMal <malakhovskaya@yahoo.com>
2024-03-10 18:41:53 -0500

    spring boot demo

---
+++ spring-06-springBoot-demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,26 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository;
+
+//    public CommentService(@Qualifier("emailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
+    public CommentService(@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
+        this.commentNotificationProxy = commentNotificationProxy;
+        this.commentRepository = commentRepository;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+
+    }
+}
