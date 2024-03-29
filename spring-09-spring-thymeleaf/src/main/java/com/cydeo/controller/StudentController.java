823e9d5b15fe32c176d18182db4a7360c3758869
Violett-M <violetta.malackowski@gmail.com>
2024-03-29 16:43:58 -0500

    Spring Thymeleaf

--- spring-09-spring-thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-spring-thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,13 +3,16 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    //@RequestMapping(value = "/register", method = RequestMethod.GET) // we don't have to provide GET method
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -17,8 +20,11 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+    //@RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
         return "student/welcome";
     }
 }
+
+// when we work with UI we only use Get or Post. You either retrieve something or post something
